0563443174d45fe5e15ed75e65e4ceb6e36001cd
Jeremy Duncan <fbcjeremy@gmail.com>
2024-04-28 21:37:27 -0400

    code correction 1

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,10 @@ import { createRoot } from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view";
 import "./index.scss";
 
-const MyFlixApplication = () => {
+const App = () => {
     return <MainView />;
 };
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
-root.render(<MyFlixApplication />);
+root.render(<App />);
