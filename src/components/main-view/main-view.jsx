8ed3a78c03552382633e41b685d6edd812bed304
Jeremy Duncan <fbcjeremy@gmail.com>
2024-09-02 14:51:23 -0400

    3.9 back to the start

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
@@ -8,135 +8,227 @@ import { ProfileView } from '../profile-view/profile-view';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  useNavigate,
+	BrowserRouter,
+	Routes,
+	Route,
+	Navigate,
+	useNavigate,
 } from 'react-router-dom';
 import axios from 'axios';
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([]);
-  const storedUser = JSON.parse(localStorage.getItem('user'));
-  const storedToken = localStorage.getItem('token');
-  const [user, setUser] = useState(storedUser ? storedUser : null);
-  const [token, setToken] = useState(storedToken ? storedToken : null);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [filteredMovies, setFilteredMovies] = useState([]);
-  const searchBar = useRef(null);
-  const movieList = useRef(null);	
-  async function getUser() {
-    try {
-      const response = await fetch(
-        `https://duncanflixapi-2df251ca79e4.herokuapp.com/users/${user.Username}`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+	const [movies, setMovies] = useState([]);
+	const storedUser = JSON.parse(localStorage.getItem('user'));
+	const storedToken = localStorage.getItem('token');
+	const [user, setUser] = useState(storedUser ? storedUser : null);
+	const [token, setToken] = useState(storedToken ? storedToken : null);
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+	async function getUser() {
+		try {
+			const response = await fetch(
+				`https://duncanflixapi-2df251ca79e4.herokuapp.com/users/${user.Username}`,
+				{
+					method: 'GET',
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
 
-      const data = await response.json();
-      setUser(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
 
-  // Add movie to users favorite list
-  async function handleAddToFavorites(movie) {
-    try {
-      const response = await fetch(
-        `https://duncanflixapi-2df251ca79e4.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + token,
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return getUser();
-    } catch (error) {
-      console.log(error);
-    }
-  }
+			const data = await response.json();
+			setUser(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  useEffect(() => {
-    if (searchBar.current && movieList.current) {
-      const handleSearch = (e) => {
-        const searchQuery = e.target.value.toLowerCase();
-        setSearchQuery(searchQuery);
-        const filteredMovies = movies.filter((movie) => {
-          return movie.title.toLowerCase().includes(searchQuery);
-        });
-        setFilteredMovies(filteredMovies);
-      };
-      searchBar.current.addEventListener('input', handleSearch);
-      return () => {
-        searchBar.current.removeEventListener('input', handleSearch);
-      };
-    }
-  }, [movies]);
+	// Add movie to users favorite list
+	async function handleAddToFavorites(movie) {
+		try {
+			const response = await fetch(
+				`https://duncanflixapi-2df251ca79e4.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: 'Bearer ' + token,
+					},
+				}
+			);
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return getUser();
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  return (
-    <Row>
-      <Col>
-        <input
-          type="text"
-          ref={searchBar}
-          placeholder="Search movies"
-          value={searchQuery}
-        />
-      </Col>
-      <Col>
-        {filteredMovies.length > 0 ? (
-          filteredMovies.map((movie) => (
-            <Col className="mb-4" key={movie._id} md={3}>
-              <MovieCard
-                user={user}
-                movie={movie}
-                onMovieClick={(newSelectedMovie) => {
-                  // handle movie click
-                }}
-                handleAddToFavorites={() => handleAddToFavorites(movie)}
-                handleRemoveFromFavorites={() =>
-                  // handle remove from favorites
-                  console.log('Remove from favorites')
-                }
-              />
-            </Col>
-          ))
-        ) : movies.length === 0 ? (
-          <Col>The list is empty!</Col>
-        ) : (
-          movies.map((movie) => (
-            <Col className="mb-4" key={movie._id} md={3}>
-              <MovieCard
-                user={user}
-                movie={movie}
-                onMovieClick={(newSelectedMovie) => {
-                  // handle movie click
-                }}
-                handleAddToFavorites={() => handleAddToFavorites(movie)}
-                handleRemoveFromFavorites={() =>
-                  // handle remove from favorites
-                  console.log('Remove from favorites')
-                }
-              />
-            </Col>
-          ))
-        )}
-      </Col>
-    </Row>
-  );
+	// Delete movie from users favorites
+	async function handleRemoveFromFavorites(movie) {
+		return await axios
+			.delete(
+				`https://duncanflixapi-2df251ca79e4.herokuapp.com/users/${user.Username}/${movie._id}`,
+				{
+					headers: { Authorization: `Bearer ${token}` },
+				}
+			)
+			.then((response) => {
+				return getUser();
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
+	async function fetchMovies(token) {
+		try {
+			const fetchedData = await fetch(
+				'https://duncanflixapi-2df251ca79e4.herokuapp.com/movies',
+				{
+					headers: { Authorization: `Bearer ${token}` },
+				}
+			);
+			if (!fetchedData.ok) {
+				throw new Error(`HTTP error! status: ${fetchedData.status}`);
+			}
+			const jsonData = await fetchedData.json();
+			const movies = jsonData.map((doc) => {
+				return {
+					_id: doc._id,
+					title: doc.Title,
+					description: doc.Description,
+					genre: {
+						name: doc.Genre.Name,
+						description: doc.Genre.Description,
+					},
+					director: {
+						name: doc.Director.Name,
+						bio: doc.Director.Bio,
+						birth: doc.Director.Birth,
+					},
+					imagepath: doc.ImagePath,
+					actors: doc.Actors,
+				};
+			});
+			setMovies(movies);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		if (!token) return;
+
+		getUser();
+		fetchMovies(token);
+	}, [token]);
+
+	return (
+		<BrowserRouter>
+			<Row className='justify-content-md-center'>
+				<NavigationBar
+					user={user}
+					onLoggedOut={() => {
+						setUser(null);
+						setToken(null);
+						localStorage.clear();
+						window.location.href = '/';
+					}}
+				/>
+				<Routes>
+					<Route
+						path='/signup'
+						element={
+							!user ? (
+								<Col className='mb-5' md={5}>
+									<SignupView />
+								</Col>
+							) : (
+								<Navigate to='/' />
+							)
+						}
+					/>
+					<Route
+						path='/login'
+						element={
+							user ? (
+								<Navigate to='/' />
+							) : (
+								<Col md={5}>
+									<LoginView
+										onLoggedIn={(user, token) => {
+											setUser(user);
+											setToken(token);
+										}}
+									/>
+								</Col>
+							)
+						}
+					/>
+					<Route
+						path='/movies/:movieId'
+						element={
+							!user ? (
+								<Navigate to='/login' replace />
+							) : movies.length === 0 ? (
+								<Col>The list is empty!</Col>
+							) : (
+								<Col className='mb-5' md={8}>
+									<MovieView movies={movies} />
+								</Col>
+							)
+						}
+					/>
+					<Route
+						path='/'
+						element={
+							!user ? (
+								<Navigate to='/login' replace />
+							) : movies.length === 0 ? (
+								<Col>The list is empty!</Col>
+							) : (
+								<>
+									{movies.map((movie) => (
+										<Col className='mb-4' key={movie._id} md={3}>
+											<MovieCard
+												user={user}
+												movie={movie}
+												onMovieClick={(newSelectedMovie) => {
+													setSelectedMovie(newSelectedMovie);
+												}}
+												handleAddToFavorites={() => handleAddToFavorites(movie)}
+												handleRemoveFromFavorites={() =>
+													handleRemoveFromFavorites(movie)
+												}
+											/>
+										</Col>
+									))}
+								</>
+							)
+						}
+					/>
+					<Route
+						path='/profile'
+						element={
+							<ProfileView
+								user={user}
+								token={token}
+								movies={movies}
+								handleAddToFavorites={handleAddToFavorites}
+								handleRemoveFromFavorites={handleRemoveFromFavorites}
+							/>
+						}
+					/>
+					actors
+				</Routes>
+			</Row>
+		</BrowserRouter>
+	);
 };
+
+export default MainView;
